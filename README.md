0ee5ad06c0f26900bc49ebd78288946430d72985
soobeenpark <soobeenpark.dev@gmail.com>
2020-09-14 23:47:11 +0900

    Add reddit credentials - PRAW can now fetch reddit posts
    praw.ini config file was added locally - never share this
    externally.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Automatically update [FRESH]est new Album / Track releases.
 Built using Python 3.8.0 (in Python venv).
 Ubuntu 18.04 LTS.
 Please refer to `requirements.txt` to view the full list of dependencies used.
+
+In order to run this script, the username, password, app client_id, and app client_secret need to be set in a separate `praw.ini` config file.
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 
 import praw
+import pprint
 
 def main():
-    print("Hello World!")
+    reddit = praw.Reddit("bot1", config_interpolation="basic")
+    print("Read-only mode?: ", reddit.read_only)
+
+    # Print hottest N posts on subreddit
+    for i, submission in enumerate(reddit.subreddit("indieheads").hot(limit=20)):
+        print(i, submission.title)
+
+    # Print out what attributes are available in "submission" object
+    pprint.pprint(vars(submission))
+
 
 
 if __name__ == "__main__":
