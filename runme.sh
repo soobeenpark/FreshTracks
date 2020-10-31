84bc91f3eedc0b05dd898834ef9d398c1406bfb2
Soobeen Park <soobeenpark.dev@gmail.com>
2020-11-01 02:37:06 +0900

    Modify venv location in runme.sh

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd 'path/name/to/deployed/'
-source ./bin/activate
+source ./venv/bin/activate
 export SPOTIPY_CLIENT_ID='MY_CLIENT_ID'
 export SPOTIPY_CLIENT_SECRET='MY_CLIENT_SECRET'
 export SPOTIPY_REDIRECT_URI='MY_REDIRECT_URI'
